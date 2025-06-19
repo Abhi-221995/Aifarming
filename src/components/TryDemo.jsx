7a3c3aea3abc94842208b59824751b8d3c304222
Abhinav Reddy Daggula <dabhinav2224@gmail.com>
2025-06-19 12:51:49 +0530

    second commit

--- src/components/TryDemo.jsx
+++ src/components/TryDemo.jsx
@@ -1,14 +1,32 @@
 import Navbar from "./Navbar";
-import Footer from './footer/Footer'
+import Footer from './footer/Footer';
+import './tryDemoStyle.css'
 
 function TryDemo(){
     return(
         <div className="demoMainContainer">
         <Navbar/>
-        <h3>this is demo page</h3>
+        <div className="demologinContainer">
+              <div className="demoCard">
+                 <div>
+                    <img src="https://aifarming.ca/wp-content/uploads/2025/01/image-18.png" alt="logo" />
+                 </div>
+                 <div>
+                    <h2>Start With email</h2>
+                    <p style={{color:"#333",padding:"20px"}}>To continue signing up,please enter your email <br />for varification with
+                    an one time code
+                    </p>
+                    <div>
+                        <label htmlFor="email">Email</label><br/>
+                        <input className="emailInput" type="email" placeholder="Email"/>
+                    </div>
+                    <button className="demobtn">Continue with Email</button>
+                 </div>
+              </div>
+        </div>
         <Footer/>
         </div>
     )
-}
+};
 
 export default TryDemo
